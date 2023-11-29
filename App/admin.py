694b15224049aeed472d8c9f675384a2730b4ed5
binnysheriff <binnysheriff@gmail.com>
2023-11-29 11:03:52 -0500

    Wip

--- App/admin.py
+++ App/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Comment, Ticket
 
 
@@ -6,6 +7,7 @@ from .models import Comment, Ticket
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
